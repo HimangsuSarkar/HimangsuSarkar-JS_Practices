648b50a7aebeb6e3769bcab421a9daa8e5ab268e
himangsu <hhimangsusarkar1@gmail.com>
2021-12-26 10:26:26 +0800

    added functionality in modal and overly

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -3,15 +3,20 @@ const modal = document.querySelector(".modal");
 const overly = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnOpenModal = document.querySelectorAll(".show-modal");
-console.log(btnOpenModal);
 
-for (let i = 0; i < btnOpenModal.length; i++)
-  btnOpenModal[i].addEventListener('click', function () {
-    console.log("clicked");
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overly.classList.add('hidden');
+}
+const openModal=function () {
     modal.classList.remove('hidden');
     overly.classList.remove('hidden');
-  })
+  }
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener('click',openModal )
 
-btnCloseModal.addEventListener('click', function () {
+
+btnCloseModal.addEventListener('click', closeModal)
   
-  })
+overly.addEventListener('click', closeModal);
