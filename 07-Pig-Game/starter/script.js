a0a9f518db532257c777ca4185fbe5011aca5c88
himangsu <hhimangsusarkar1@gmail.com>
2022-01-03 00:36:02 +0800

    rolling funcationality

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,11 +1,42 @@
 'use strict';
 
-const score0El = document.getElementById('score--0');
+//selecting elements
+const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById('score--1');
-const dice = document.querySelector('.dice');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
 
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector(".btn--hold");
+
+
+//initial 
 score0El.textContent = 0;
 score1El.textContent = 0;
 
-dice.classList.add('hidden');
+diceEl.classList.add('hidden');
+let currentScore = 0;
+
+//rolling functionality
+btnRoll.addEventListener('click', function () {
+  //1.generating random number
+  const dice =Math.trunc(Math.random()*6)+1;
+  console.log(dice);
+
+  //2.display 
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //3.check if it 1 ;true switch otherwise add to current
+  if (dice !== 1) {
+    //add to current score
+    currentScore = currentScore + dice;
+    current0El.textContent = currentScore;
+
+  } else {
+    //switch player
+  }
+})
 
