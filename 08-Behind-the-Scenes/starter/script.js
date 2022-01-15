a54a204c15f9b9a3e0b6ac3024ad996557cd33a6
himangsu <hhimangsusarkar1@gmail.com>
2022-01-15 12:38:36 +0800

    practice scope chain

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+function calcAge(birthday) {
+  const age = 2022 - birthday;
+  
+  function printAge() {
+    const output = `${firstName},you are ${age}, born in ${birthday}`;
+    console.log(output);
+
+    if (birthday >= 1981 && birthday <= 1996) {
+      var millenial = true;
+      const str = `oh! and you are a millenial, ${firstName}`;
+      console.log(str);
+    }
+  }
+  printAge();
+  return age;
+  
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
+// console.log(age);
+//printAge();
