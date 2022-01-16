db408bc590ef5192b8b1b6b4d441b784a85878b1
himangsu <hhimangsusarkar1@gmail.com>
2022-01-16 21:52:57 +0800

    scope chin discuss

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,23 +1,37 @@
 'use strict';
-function calcAge(birthday) {
-  const age = 2022 - birthday;
-  
-  function printAge() {
-    const output = `${firstName},you are ${age}, born in ${birthday}`;
-    console.log(output);
 
-    if (birthday >= 1981 && birthday <= 1996) {
-      var millenial = true;
-      const str = `oh! and you are a millenial, ${firstName}`;
+function calcAge(birthYear) {
+  const age = 2022 - birthYear;
+  // console.log(firstName);
+
+  function printAge() {
+    
+    let output = `${firstName} ,you age is ${age} ,your birth year ${birthYear}`;
+    console.log(output);
+    
+    if (birthYear >= 1986 && birthYear <= 1996) {
+      let firstName = 'Steven';
+      var millioner = true;
+      const str = `oh!${firstName} you are a millioner`;
       console.log(str);
+
+      function add(a,b) {
+        return a + b;
+      }
+      console.log(add(2, 3));
+      const output = 'new output';
+      console.log(output)
     }
+    
+    console.log(millioner);
+    // console.log(str);
   }
+  // console.log(output);
   printAge();
   return age;
-  
+
 }
 
 const firstName = 'Jonas';
 calcAge(1991);
-// console.log(age);
-//printAge();
+
